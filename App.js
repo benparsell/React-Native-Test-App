97d38956a1be95a2c2f5a48e3d601733d9493b37
Ben Parsell <ben.parsell1507@gmail.com>
2018-03-19 00:07:59 -0400

    Added API foundation.. still buggy

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
+
+import axios from 'axios';
+
+const API_KEY = "5ff84e4b92ce4f8f";
 
 export default class App extends React.Component {
+  temp_high = null;
+  _getForecast(zipcode)
+  {
+    const request = "http://api.wunderground.com/api/" + API_KEY + "/forecast/q/" + zipcode + ".json";
+    axios.get(request).then( (response) => {
+        if(response.status == 200) {
+            this.response = response.data;
+            return this.response.data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+        }
+
+    });
+  }
+
   render() {
+    this.temp_high = this._getForecast(49306);
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Weather for Belmont, MI</Text>
+        <Text>High: {this.temp_high}</Text>
+        <Text></Text>
       </View>
     );
   }
