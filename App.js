25606b6a7ca17db4d7321fc1af8021e15ba05f9d
Ben Parsell <ben.parsell1507@gmail.com>
2018-03-19 23:05:08 -0400

    API works, and pulls high temp for default ZipCode.

--- App.js
+++ App.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, Image, View } from 'react-native';
 
 import axios from 'axios';
 
 const API_KEY = "5ff84e4b92ce4f8f";
+const DEFAULT_ZIPCODE = 49401;
 
 export default class App extends React.Component {
-  temp_high = null;
+
+  constructor() {
+    super();
+    this.state = {
+      zipcode: DEFAULT_ZIPCODE,
+      temp_high: null,
+    }
+  }
+
   _getForecast(zipcode)
   {
     const request = "http://api.wunderground.com/api/" + API_KEY + "/forecast/q/" + zipcode + ".json";
-    axios.get(request).then( (response) => {
+    return axios.get(request).then( (response) => {
         if(response.status == 200) {
-            this.response = response.data;
-            return this.response.data.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+            var weather = response.data.forecast.simpleforecast.forecastday;
+            var temp = weather[0].high.fahrenheit;
+            this.setState({temp_high: temp});
         }
-
     });
   }
 
   render() {
-    this.temp_high = this._getForecast(49306);
+    this._getForecast(this.state.zipcode);
+
     return (
       <View style={styles.container}>
         <Text>Weather for Belmont, MI</Text>
-        <Text>High: {this.temp_high}</Text>
+        <Text>High: {this.state.temp_high}</Text>
         <Text></Text>
       </View>
     );
