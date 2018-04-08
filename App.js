4e62ec1e247545809a5c6fc6176331515439c2e0
tyleranson <ansont@mail.gvsu.edu>
2018-04-08 13:56:10 -0400

    Auth Portion

--- App.js
+++ App.js
@@ -1,61 +1,109 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, TextInput, Button, Image, View } from 'react-native';
+import { Picker, TextInput, Alert, Button, StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
 
-import axios from 'axios';
-
-const API_KEY = "5ff84e4b92ce4f8f";
-const DEFAULT_ZIPCODE = 49401;
 
 export default class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      zipcode: DEFAULT_ZIPCODE,
-      temp_high: null,
-      temp_low: null,
+  state = {
+    zipcode: '',
+    userEmail: '',
+    userPass: '',
+    fname: ''
+  };
+
+  AddToDB(){
+ 
+    var user = firebase.auth().currentUser;
+
+    if( user != null){
+        firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/zipcodes').push({
+        zipcode: this.state.zipcode
+      }); 
     }
+    else{
+      Alert.alert("You must be signed in to save a zipcode.");
+    }
+       
   }
 
-  _getForecast(zipcode)
-  {
-    const request = "http://api.wunderground.com/api/" + API_KEY + "/forecast/q/" + zipcode + ".json";
-    return axios.get(request).then( (response) => {
-        if(response.status == 200) {
-            var weather = response.data.forecast.simpleforecast.forecastday;
-            var high_temp = weather[0].high.fahrenheit;
-            var low_temp = weather[0].low.fahrenheit;
-            this.setState({temp_high: high_temp});
-            this.setState({temp_low: low_temp});
-        }
+  logout(){
+    firebase.auth().signOut();
+    Alert.alert("Logged Out");
+  }
+
+
+  createAccount(){
+    firebase.auth().createUserWithEmailAndPassword(this.state.userEmail, this.state.userPass).then(function(user){
+      //Alert.alert("all is well");
+      Alert.alert(user.uid)
+      //writeUserData(user.uid);
+    }).catch(function(error) {
+      
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      Alert.alert(errorMessage); 
+  });
+
+
+
+
+  }
+
+  login(){
+
+  firebase.auth().signInWithEmailAndPassword(this.state.userEmail, this.state.userPass).then(function(user){
+    // search for the zipcodes
+    
+    var ref = firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/zipcodes/');
+    ref.on("child_added", function(snapshot) {
+      var zips = snapshot.val();
+      Alert.alert(zips.zipcode);
+    }, function (errorObject) {
+      Alert.alert("The read failed: " + errorObject.code);
     });
-  }
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+    Alert.alert(errorMessage)
 
-  _handlePress(zip)
-  {
-    if(zip.length == 5) {
-      this.setState({zipcode: zip})
-      this._getForecast(this.state.zipcode);
-    }
+    // ...
+  });
 
-  }
+}
 
   render() {
-    this._getForecast(this.state.zipcode);
-
     return (
       <View style={styles.container}>
-        <TextInput placeholder="Enter Zipcode"
-          returnKeyLabel = {"next"}
-          onChangeText={this._handlePress.bind(this)}/>
-
-        <Text>Weather for {this.state.zipcode}</Text>
-        <Text>High: {this.state.temp_high}</Text>
-        <Text>Low: {this.state.temp_low}</Text>
+         <TextInput
+        placeholder="Name"
+        onChangeText={(value) => this.setState({fname: value})}
+        />{"\n"}{"\n"}{"\n"}
+        <TextInput
+        placeholder="Zip Code"
+        onChangeText={(value) => this.setState({zipcode: value})}
+        />{"\n"}{"\n"}{"\n"}
+        <TextInput
+        placeholder="Email"
+        onChangeText={(value) => this.setState({userEmail: value})}
+        />{"\n"}{"\n"}{"\n"}
+        <TextInput
+        placeholder="Password"
+        onChangeText={(value) => this.setState({userPass: value})}
+        />{"\n"}{"\n"}{"\n"}
+        
+        <Button onPress={ this.AddToDB.bind(this) } title="Save Zipcode" />{"\n"}
+        <Button onPress={ this.login.bind(this) } title="Sign In" />{"\n"}
+        <Button onPress={ this.logout.bind(this) } title="Sign Out" />{"\n"}
+        <Button onPress={ this.createAccount.bind(this) } title="Create Account" />
       </View>
     );
   }
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -65,3 +113,40 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBH4YejbRQoEo5ThbCMHj6dbajHlIWeHZo",
+  authDomain: "weatherdb-814eb.firebaseapp.com",
+  databaseURL: "https://weatherdb-814eb.firebaseio.com",
+  storageBucket: "weatherdb-814eb.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    
+    Alert.alert("Logged In");
+
+    //var user = firebase.auth().currentUser;
+
+    //if(user != null){
+
+      //var email_id = user.email;
+      //document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    //}
+
+  //} else {
+    // No user is signed in.
+
+    //document.getElementById("user_div").style.display = "none";
+    //document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+
